f3cd3ad5272eb49f24b73cd4df5bee2750f1dc55
AshrafHofny <ashraf.hofny@blink22.com>
2019-10-14 15:21:37 +0200

    my module

--- addons/my_module/__manifest__.py
+++ addons/my_module/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "sccc",
+    'name': "my_module",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -16,7 +16,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Test',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
