779e087c6e79eb2e79865beea2d8e51cebe7730e
AshrafHofny <ashraf.hofny@blink22.com>
2019-10-17 15:09:00 +0200

    structure views

--- addons/sccc/models/time_slots.py
+++ addons/sccc/models/time_slots.py
@@ -2,7 +2,8 @@ from odoo import models, fields, api
 class TimeSlots(models.Model):
   _name = 'sccc.time_slots'
   name = fields.Char('Name')
-  time = fields.Float('Name')
+  from_time = fields.Float('From')
+  to_time = fields.Float('To')
   day_of_week = fields.Char('Day of Week')
 
   # Relations
