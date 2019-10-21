855a19f1e0cfb3663afb0af2b4c02bd0ec4f3ecc
AshrafHofny <ashraf.hofny@blink22.com>
2019-10-21 13:32:49 +0200

    staff fields

--- addons/sccc/models/counselors.py
+++ addons/sccc/models/counselors.py
@@ -12,11 +12,20 @@ class Counselors(models.Model):
     departments = fields.Selection([('Administration', 'Administration'), ('Counselors', 'Counselors'),
                                     ('Directors', 'Directors'), ('Front Desk', 'Front Desk'),
                                     ('Psychiatry', 'Psychiatry'), ('Coordinators', 'Coordinators')], 'Departments')
+
+    ethnicity = fields.Selection([('American Indian/Native American/Alaskan Native', 'American Indian/Native American/Alaskan Native'),
+                                ('Asian', 'Asian'), ('Black', 'Black'), ('Latina/o/x', 'Latina/o/x'),
+                                ('Middle Eastern or North African','Middle Eastern or North African'), ('Mixed','Mixed'), 
+                                ('Native Hawaiian or Pacific Islander','Native Hawaiian or Pacific Islander'), 
+                                ('White','White'), ('Other','Other'), ('Declines to Specify','Declines to Specify')], 'Ethnicity')
+
+    language = fields.Char('Language')
     created_on = fields.Datetime("Date")
     
     # Relations
     files = fields.Many2many('sccc.file', 'counselor_file_rel', string='Case Load')
     fam_assessment = fields.One2many('sccc.fam_assessment', 'counselor', string='Fam Assessment')
+    location = fields.One2many('sccc.location', 'counselor', string='Location')
 
     @api.depends('last_name', 'first_name')
     def _set_name(self):
