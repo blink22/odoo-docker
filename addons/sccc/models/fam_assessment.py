e5b21a3eabb9d1a840db3ba516ef959a58d96b48
AshrafHofny <ashraf.hofny@blink22.com>
2019-11-05 15:02:00 +0200

    [SCCC-63]

--- addons/sccc/models/fam_assessment.py
+++ addons/sccc/models/fam_assessment.py
@@ -5,15 +5,15 @@ from odoo import models, fields, api
 class FamAssessment(models.Model):
   _name = 'sccc.fam_assessment'
   _description = 'Family Assessments'
+  _rec_name = 'combination'
+  combination = fields.Char(string='Name', compute='_compute_fields_combination')
 
-  name = fields.Char('Name')
   intake = fields.Binary('Upload Intake Form')
-  date = fields.Date('Date & Time')
-  therapy_type = fields.Selection([('a', 'A'), ('b', 'B')], 'What type of therapy is this?')
+  date = fields.Date('Intake Date')
+  therapy_type = fields.Selection([('Family', 'Family'), ('Couple', 'Couple'), ('Child','Child')], 'Therapy Type')
   language_needs = fields.Text('Does family/couple have any language needs?')
-  individual_counseling = fields.Selection([('yes', 'Yes'), ('no', 'No')], 'Family/Couple members received individual counseling?')
-  currency_id = fields.Integer(compute='_get_currency', store=True)
-  outstanding_balance = fields.Monetary('Any outstanding balance?')
+  
+  outstanding_balance = fields.Float('Any outstanding balance? (Must be $0 for assignment)')
   mandated_therapy = fields.Selection([('yes', 'Yes'), ('no', 'No')], 'Is client mandated to attend therapy?')
   mandated_therapy_agency = fields.Text('If yes, which agency?')
   # Is this right?
@@ -27,21 +27,17 @@ class FamAssessment(models.Model):
   
   #
   appointment_date = fields.Date('Date/time of appointment')
-  fee = fields.Monetary('Fee')
+  fee = fields.Float('Fee')
   additional_notes = fields.Text('Additional notes')
   created_on = fields.Datetime("Date")
 
   # Relations
   file = fields.One2many('sccc.file', 'fam_assessment', string='File')
-  client = fields.Many2one('sccc.client', 'Client')
-  provider = fields.Many2one('sccc.provider', string='provider')
+  clients = fields.Many2many('sccc.client', 'fam_assessment_clients_rel', string='Family/Couple members received individual counseling?')
+  provider = fields.Many2one('sccc.provider', string='Intake Provider')
   # assigned_to = fields.Many2many('sccc.provider', string='If so, who were they assigned to?')
 
-  def _get_currency(self):
-    user_obj = self.pool.get('res.users')
-    currency_obj = self.pool.get('res.currency')
-    user = user_obj.browse(cr, uid, uid, context = context)
-    if user.company_id:
-        self.currency_id = user.company_id.currency_id.id
-    else:
-        self.currency_id = currency_obj.search(cr, uid, [('rate', '=', 1.0)])[0]
+  @api.depends('file', 'date') 
+  def _compute_fields_combination(self):
+    for fam in self:
+      fam.combination = fam.combination + ' Family Assessment ' + fam.date
