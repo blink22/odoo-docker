ab81c4dc77bc1705464cd90c6cd2d7ba6f083909
AshrafHofny <ashraf.hofny@blink22.com>
2019-11-10 15:50:33 +0200

    [SCCC-75] Render all locations

--- addons/web_view_calendar_column/static/src/js/calendar_renderer.js
+++ addons/web_view_calendar_column/static/src/js/calendar_renderer.js
@@ -65,6 +65,9 @@ odoo.define('web_view_calendar_column.CalendarRenderer', function (require) {
                         }
                         count = 0;
                     }
+                    self.state.filters.location.filters.forEach(filter => {
+                        filter.display = true;
+                    });
                 },
                 viewRender: function (view) {
                     // compute mode from view.name which is either 'month', 'agendaWeek' or 'agendaDay'
